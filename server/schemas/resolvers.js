72a4a6fca02b63367c8ac2b797c5449d4fd8a30e
PsychoRSS <Bobgonzalez516@gmail.com>
2023-11-11 02:03:44 -0700

    added resolvers and mutations

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,20 @@
+const { User} = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+    Query :{
+        user: async () => {
+            return User.findOne()
+        },
+        Book: async (parent, { username }) => {
+            return User.findOne({username}).populate('savedBooks')
+        },
+        me: async (parent, args, context) => {
+            return User.findOne({id: context.user.id})
+        }
+    },
+
+    Mutaions: {
+
+    }
+}
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,43 @@
-22
+const typeDefs = `
+type User {
+    id: ID!
+    username: String!
+    email: String!
+    bookCount: String
+    savedBooks: [Book]!
+  }
+
+type Book {
+    bookId: ID
+    authors: [String]!
+    description: String!
+    title: String!
+    image: Stirng!
+    link: String!
+}
+
+type Auth {
+    token: ID!
+    user: User
+  }
+
+type Query {
+    me: User
+}
+
+input bookInput {
+    authors: [String!]!
+    description: String!
+    title: String!
+    bookId: String!
+    image: String!
+    link: String!
+  }
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username:String!, email: String!, password: String!): Auth
+    saveBook(input: bookInput!): User
+    removeBook(bookId: ID): User 
+}
+`
